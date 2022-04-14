644bfd0445708421f7a67cc0b524fbdd6e900295
sarahaguasvivas <saag5228@colorado.edu>
2022-04-14 12:15:34 -0600

    fixed bug from converting back

--- helpers/bitset.py
+++ helpers/bitset.py
@@ -1,10 +1,17 @@
 import numpy as np
 import struct
-from bitarray.util import serialize, deserialize, ba2int, vl_decode
+from bitarray.util import  count_xor
 from bitarray import bitarray
 import typing
 from nptyping import Float16
 from codecs import decode
+
+def float_to_bin(num):
+    return format(struct.unpack('!I', struct.pack('!f', num))[0], '032b')
+
+def bin_to_float(binary):
+    return struct.unpack('!f',struct.pack('!I', int(binary, 2)))[0]
+
 
 # NOTE(sarahaguasvivas): Bitset represents the
 #           keys to my hash maps using the encodings.
@@ -18,17 +25,20 @@ class BitSet:
 
     def update_float(self, x : np.float16 = 0.0):
         self.floating_point = np.float16(x)
-        self.binary = bitarray('0' + bin(np.float16(x).view('H'))[2:])
+        bin_str = float_to_bin(float(x)) #bin(np.float16(x).view('H'))[2:].zfill(16)
+        self.binary = bitarray(bin_str, endian = 'little')
 
     def update_binary(self, x : bitarray):
         self.binary = x
-        self.floating_point = np.float16(int(x.to01()[::-1], 2))
+        self.floating_point = bin_to_float(x.to01()) #np.float16(int(x.to01(), 2))
 
 if __name__ == '__main__':
     # declare my float as a bitset
-    bs1 = BitSet(2.0)
+    bs1 = BitSet(10.0)
     bs2 = BitSet(4.0)
     print(bs1.binary)
     print(bs2.binary)
     bs1.update_binary(bs1.binary)
     print(bs1.floating_point)
+    print(bs1.binary ^ bs2.binary)
+    print(count_xor(bs1.binary, bs2.binary))
