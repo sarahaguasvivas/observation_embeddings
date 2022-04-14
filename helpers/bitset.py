3c952464554a3a198492a16a59c7f318924f901a
sarahaguasvivas <saag5228@colorado.edu>
2022-04-14 11:57:06 -0600

    finally bitarrays

--- helpers/bitset.py
+++ helpers/bitset.py
@@ -1,27 +1,34 @@
 import numpy as np
+import struct
+from bitarray.util import serialize, deserialize, ba2int, vl_decode
+from bitarray import bitarray
 import typing
 from nptyping import Float16
+from codecs import decode
 
 # NOTE(sarahaguasvivas): Bitset represents the
 #           keys to my hash maps using the encodings.
 #           It is a container class (similar to an union in
 #           embedded)
 class BitSet:
-    def __init__(self, number : Float16):
+    def __init__(self, float_val : np.float16 = 0.0):
+        self.floating_point = None
+        self.binary : bitarray = None
+        self.update_float(float_val)
 
-        # this is the human-readable value
-        self.float_val = number
+    def update_float(self, x : np.float16 = 0.0):
+        self.floating_point = np.float16(x)
+        self.binary = bitarray('0' + bin(np.float16(x).view('H'))[2:])
 
-        # this is the serialized bitset as a string
-        self.bitset_ser = bin(np.float16(number).view('H'))[2:]\
-                            .zfill(16)
-        self.bitset_ser = int(self.bitset_ser)
-        ## this is the bitset as a byte array
-        #self.bitarray = []
-        #for i in range(16):
-        #    self.bitarray += int(self.bitset_ser[i])
+    def update_binary(self, x : bitarray):
+        self.binary = x
+        self.floating_point = np.float16(int(x.to01()[::-1], 2))
 
 if __name__ == '__main__':
     # declare my float as a bitset
-    o = BitSet(21.5)
-    print(o.bitset_ser)
+    bs1 = BitSet(2.0)
+    bs2 = BitSet(4.0)
+    print(bs1.binary)
+    print(bs2.binary)
+    bs1.update_binary(bs1.binary)
+    print(bs1.floating_point)
